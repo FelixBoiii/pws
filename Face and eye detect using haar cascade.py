c5ba8c1376b8f7636823abce9811ed577588daa2
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2021-09-11 23:35:42 +0200

    werkt!

--- Face and eye detect using haar cascade.py
+++ Face and eye detect using haar cascade.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
+import tensorflow as tf
+from tensorflow import keras
+
 
 face_cascade = cv2.CascadeClassifier(
     r"E:\Users\Bart\Documents\GitHub\pws\haarcascade_frontalface_default.xml"
@@ -8,6 +11,8 @@ face_cascade = cv2.CascadeClassifier(
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cropped_image = 0
 resized = 0
+
+model_1 = keras.models.load_model("model_1")
 
 while 1:
     ret, img = cap.read()
@@ -20,6 +25,19 @@ while 1:
         roi_color = img[y: y + h, x: x + w]
         cropped_image = gray[y: y + h, x: x + w]
         resized = cv2.resize(cropped_image, [100, 100])
+
+    #
+    test_image = (resized[..., ::-1].astype(np.float32)) / 255.0
+    #test_image = resized[..., ::-1] / 255.0
+    img_array = np.expand_dims(test_image, axis=-1)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # print(img_array)
+    prediction = model_1.predict(img_array)
+    # print(prediction)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, str(prediction), (10, 450), font,
+                3, (0, 0, 255), 2, cv2.LINE_AA)
 
     cv2.imshow("img", img)
     cv2.imshow("img2", resized)
